053e172ca44dae3504b0e25db43504881dc019f2
sushantasahoo <sushanta.biz@gmail.com>
2015-04-25 15:01:04 -0700

    Converted web-controllers to REST-controllers. Updated dependenies for Spring, Spring-boot, slf4j and aspectj.

--- SnippetShare/src/main/java/edu/sjsu/cmpe275/UserController.java
+++ SnippetShare/src/main/java/edu/sjsu/cmpe275/UserController.java
@@ -10,11 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@RestController
 public class UserController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
